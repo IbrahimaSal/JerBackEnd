01f631a7b20bc68638e83baf236fbaf41da46e7c
SALL <sallibrahima1992@gmail.com>
2021-08-28 21:07:24 +0200

    firstcommit

---
+++ src/main/java/MetisseCoreCorp/JerBackEnd/JerBackEndApplication.java
@@ -0,0 +1,11 @@
+package MetisseCoreCorp.JerBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerBackEndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JerBackEndApplication.class, args);
+	}
+}
---
+++ src/test/java/MetisseCoreCorp/JerBackEnd/JerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package MetisseCoreCorp.JerBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
